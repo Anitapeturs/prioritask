41d728d10f95b63f96fcf13e1169236283717474
anitaastros <70915108+anitaastros@users.noreply.github.com>
2024-01-31 22:56:01 +0000

    basic handling of requests

--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -9,9 +9,28 @@ USERS.get('/', (req, res, next) => {
     })
 })
 
+USERS.post('/', (req, res, next) => {
+
+    res.status(200).json({
+        message: 'handling POST requests to /user'
+    })
+})
 
 USERS.get('/:id', (req, res, next) => {
 
+    const userid = req.params.id;
+
+    if(userid === 'special'){
+        res.status(200).json({
+            message: 'you discovered the special id:',
+            id: userid
+            
+        })
+    } else {
+        res.status(200).json({
+            message : 'you passed an id'
+        })
+    }
 
     // Tip: All the information you need to get the id part of the request can be found in the documentation 
     // https://expressjs.com/en/guide/routing.html (Route parameters)
@@ -33,12 +52,16 @@ USERS.post('/', (req, res, next) => {
 
     
 
-USERS.put('/:id', (req, res) => {
-    /// TODO: Edit user
+USERS.put('/:id', (req, res, next) => {
+    res.status(200).json({
+        message: 'handling PUT requests to /user'
+    })
 })
 
-USERS.delete('/:id', (req, res) => {
-    /// TODO: Delete user.
+USERS.delete('/:id', (req, res, next) => {
+    res.status(200).json({
+        message: 'handling DELETE requests to /user'
+    })
 })
 
 export default USERS;
